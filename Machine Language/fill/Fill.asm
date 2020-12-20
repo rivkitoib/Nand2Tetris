6cecade4de793556752ed0e116464b7ab078d759
rivto <41346932+rivkitoib@users.noreply.github.com>
2020-12-20 15:47:39 +0200

    Add asm implementation for fill and mult

--- Machine Language/fill/Fill.asm
+++ Machine Language/fill/Fill.asm
@@ -10,5 +10,176 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
 // Put your code here.
+
+	
+(CHECK)
+	@background
+	M=0
+
+	@KBD
+	D=M
+
+	@COLOR
+	D;JEQ
+	   	 
+	@background
+	M=-1
+	
+	//spirala
+(COLOR)	
+	@32
+	D=A
+	@columns
+	M=D //columns=32
+
+	@256
+	D=A
+	@rows
+	M=D //rows=256
+
+	@SCREEN
+	D=A
+	@adress
+	M=D //adress=SCREEN
+	
+
+
+(LOOP)
+	@index
+	M=0 //index=0
+	(TOP)
+		@background
+		D=M
+		@adress
+		A=M
+		M=D //change sreen
+
+		@adress
+		M=M+1 //adress++
+		
+		@index
+		M=M+1 //index++
+		D=M
+		
+		// while( index < columns ) continue; 
+		@columns
+		D=M-D
+		@TOP
+		D;JGT
+
+	@adress
+	M=M-1 //adress--
+	@index
+	M=0 //index=0
+	(RIGHT)
+		@background
+		D=M
+		@adress
+		A=M
+		M=D //change sreen
+
+		@32
+		D=A
+		@adress
+		M=D+M //adress+=32
+
+		@index
+		M=M+1 //index++
+		D=M
+		
+		// while( index < rows ) continue; 
+		@rows
+		D=M-D
+		@RIGHT
+		D;JGT
+
+	@32
+	D=A
+	@adress
+	M=M-D //adress-=32
+	@index
+	M=0 //index=0
+	(BOTTOM)
+		@background
+		D=M
+		@adress
+		A=M
+		M=D //change sreen
+
+		@adress
+		M=M-1 //adress--
+		
+		@index
+		M=M+1 //index++
+		D=M
+		
+		// while( index < columns ) continue; 
+		@columns
+		D=M-D
+		@BOTTOM
+		D;JGT
+	@adress
+	M=M+1 //adress++
+	@index
+	M=0 //index=0
+	(LEFT)
+		@background
+		D=M
+		@adress
+		A=M
+		M=D //change sreen
+
+		@32
+		D=A
+		@adress
+		M=M-D //adress-=32
+
+		@index
+		M=M+1 //index++
+		D=M
+		
+		// while( index < rows ) continue; 
+		@rows
+		D=M-D
+		@LEFT
+		D;JGT
+
+	@32
+	D=A
+	@adress
+	M=M+D //adress+=32
+	M=M+D //adress+=32
+	M=M+1//adress++
+
+	//update values
+	@columns
+	M=M-1
+	M=M-1 //columns-=2
+	@rows
+	M=M-1
+	M=M-1 //rows-=2
+
+	@rows
+	D=M
+	
+
+	@LOOP
+	D;JGT
+	@CHECK
+	0;JMP
+
+(END)
+@END
+ 0;JMP
+
+
+
+	
+
+
+
+
+
+
+
--- Machine Language/mult/mult.asm
+++ Machine Language/mult/mult.asm
@@ -7,3 +7,51 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+	@R0
+	D=M
+	@num
+	M=D //num=R0
+
+	@R1
+	D=M
+	@multiplier
+	M=D //multiplier=R1
+
+	@result
+	M=0 //result=0
+
+	@i
+	M=0 //i=0
+(LOOP)
+	@i
+	D=M
+	M=M+1 //i++
+	@multiplier
+	D=M-D
+	//if (i==multiplier) goto END
+	@PUT_RES
+	D;JEQ
+
+	@num
+	D=M
+	@result
+	M=M+D
+	@LOOP
+	0;JMP
+
+(PUT_RES)
+	@result
+	D=M
+	@R2
+	M=D //R2=result
+	@END
+	0;JMP
+
+(END)
+	@END
+	0;JMP
+
+
+	
